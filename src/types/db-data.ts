3aab208b9febcf50d17bf1a7968c94f4dbc931ad
Isaac León <isaacmlp714@gmail.com>
2024-06-21 13:56:53 +0100

    Add new section to students

--- src/types/db-data.ts
+++ src/types/db-data.ts
@@ -4,10 +4,18 @@ export interface DataSections {
 
 export interface Data {
   sections: Section[]
+  students: Student[]
 }
 
 export interface Section {
   id: string
+  name: string
+  baseURL: string
+}
+
+export interface Student {
+  id: string
+  idSection: string
   name: string
   baseURL: string
 }
